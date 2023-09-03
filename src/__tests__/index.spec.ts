cb2eadb08066b6fc2d7f8eb990363e2d4ffe6770
eduardor2m <deveduardomelo@gmail.com>
2023-09-03 02:10:04 -0300

    :test_tube: test: add tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,10 +1,19 @@
-import { Student } from '..';
+import { searchLinear, Student } from '..';
 
 describe('index.ts', () => {
-  it('should be true', () => {
+  it('should be true in the class Student', () => {
     const student = new Student('John', 20);
     student.addGrade(10);
     expect(student.getMedia()).toBe(10);
     expect(student.getName()).toBe('John');
+  });
+
+  it('should be true in the function searchLinear', () => {
+    var array = [
+      30, 15, 22, 10, 5, 8, 16, 60, 40, 70, 80, 90, 100, 20, 25, 35, 45, 55, 65, 75, 85, 95, 99, 1,
+      2, 3, 4, 5, 6, 7, 8, 9, 10,
+    ];
+    expect(searchLinear(array, 10)).toBe(3);
+    expect(searchLinear(array, 1000)).toBe(-1);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -35,9 +35,33 @@ export class Student implements IStudent {
   }
 }
 
-const student = new Student('John', 20);
+var array = [
+  30, 15, 22, 10, 5, 8, 16, 60, 40, 70, 80, 90, 100, 20, 25, 35, 45, 55, 65, 75, 85, 95, 99, 1, 2,
+  3, 4, 5, 6, 7, 8, 9, 10,
+];
 
-student.addGrade(10);
+export function searchLinear(array: number[], value: number) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) return i;
+  }
+  return -1;
+}
 
-console.log(student.getMedia());
-console.log(student.getName());
+export function bubbleSort(array: number[]) {
+  let swapped = false;
+  do {
+    swapped = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      const current = array[i];
+      const next = array[i + 1];
+      if (current > next) {
+        array[i] = next;
+        array[i + 1] = current;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return array;
+}
+
+console.log(bubbleSort(array));
